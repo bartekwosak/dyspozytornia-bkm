967c0da14127b8ea3b3abdb12c528f9a6625c2be
Bartłomiej Osak <barosa1996@hotmail.com>
2018-05-15 22:44:26 +0200

    Added new migrate with driver table.

---
+++ database/migrations/2018_05_14_201825_create_driver_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriverTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driver', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('numer_sluzbowy');
+            $table->string('imie_kierowcy');
+            $table->string('nazwisko_kierowcy');
+            $table->string('etat');
+            $table->integer('dni_pracy');
+            $table->integer('stalka');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driver');
+    }
+}
