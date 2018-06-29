fcc61986dc6df62138415d134b9f0c33d5b0ed34
Bartłomiej Osak <barosa1996@hotmail.com>
2018-06-29 23:22:49 +0200

    Added RestApi routes.
    Signed-off-by: Bartłomiej Osak <barosa1996@hotmail.com>

--- app/Http/Controllers/BrigadesController.php
+++ app/Http/Controllers/BrigadesController.php
@@ -9,6 +9,7 @@ use Auth;
 use Datatables;
 use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Session;
 
 class BrigadesController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', function () {
         return view('welcome');
@@ -25,10 +28,26 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('deleteAllBrigades', 'BrigadesController@deleteAll');
     Route::get('deleteAllDrivers', 'DriverController@deleteAll');
 
-    Route::get('archiveGraphic','ArchiveTablesController@archiveGraphic');
-    Route::get('archiveBrigades','ArchiveTablesController@archiveBrigades');
-    Route::get('archiveDrivers','ArchiveTablesController@archiveDrivers');
+    Route::get('archiveGraphic', 'ArchiveTablesController@archiveGraphic');
+    Route::get('archiveBrigades', 'ArchiveTablesController@archiveBrigades');
+    Route::get('archiveDrivers', 'ArchiveTablesController@archiveDrivers');
 });
 
+/* API Routes */
 
+Route::get('brigadesApi/{line}/{brigadeVariant}/{change}', function ($line, $brigadeVariant, $change) {
+    $brigadeNumber = $line.'/'.$brigadeVariant.'/'.$change;
+    $brigade = \App\Brigade::where('numer_brygady',$brigadeNumber)->get();
+    return response()->json([$brigadeNumber => $brigade], 200);
+});
+
+Route::get('driversApi/drivers', function () {
+    $drivers = \App\Driver::all();
+    return response()->json($drivers, 200);
+});
+
+Route::get('driversApi/{service_number}', function ($service_number) {
+    $driver = \App\Driver::where('numer_sluzbowy', $service_number)->get();
+    return response()->json([$service_number => $driver], 200);
+});
 
